fdf438ec611769e6699575e64a7e09d8987ed7c3
korolovr <98011346+korolovr@users.noreply.github.com>
2022-03-26 13:23:13 -0700

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener('click',startGame)
+function startGame() {
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
