ff732b26a0a566cb1b13ced210e8949c56c202d2
korolovr <98011346+korolovr@users.noreply.github.com>
2022-03-26 19:38:54 -0700

    update
    finished base game - need to customize it for our purpose

--- script.js
+++ script.js
@@ -5,22 +5,26 @@ const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
-let totalPoints
+
 
 startButton.addEventListener('click',startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
 function startGame() {
   startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random - .5)
   currentQuestionIndex = 0
-  totalPoints = 0
+
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 
 }
 
 function setNextQuestion() {
-  restateState()
+  resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
@@ -38,35 +42,43 @@ function showQuestion(question) {
   })
 }
 
-function restateState() {
+function resetState() {
   nextButton.classList.add('hide')
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-    
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if(correct) {
-    element.classList.add('correct')
-  } else {
-    element.classList.add('wrong')
   }
-}
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
 }
 
 function selectAnswer(e) {
   const selectedButton = e.target
   const correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach( button => (
-      setStatusClass(button, button.dataset.correct)
-    ))
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  }
+  else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
   }
 }
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
 const questions = [
 
   {
